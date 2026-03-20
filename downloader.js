b392417fc5deabc0fd0cdb17e70acecf7c23588b
Momin Khan <220353398+MMN-Codes@users.noreply.github.com>
2026-03-20 20:17:32 +0530

    Update downloader.js

--- downloader.js
+++ downloader.js
@@ -2,8 +2,10 @@ const axios = require('axios');
 
 async function getDownloadLink(url) {
   try {
-    const res = await axios.get(`https://api.vevioz.com/api/button/mp4?url=${encodeURIComponent(url)}`);
-    
+    const res = await axios.get(
+      `https://api.vevioz.com/api/button/mp4?url=${encodeURIComponent(url)}`
+    );
+
     return res.data;
   } catch (err) {
     console.log(err);
